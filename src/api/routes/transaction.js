4dc924b9dea4d75a54b9ad875e641965367e3825
yusufsina <yusufsinayildiz@gmail.com>
2022-05-01 21:31:26 +0300

    added comments, fixed csv saving

--- src/api/routes/transaction.js
+++ src/api/routes/transaction.js
@@ -5,9 +5,10 @@ const { TREASURY_WALLET, USDT } = require('../../config');
 
 module.exports = (route) => {
   route.get("/transaction", async (req, res) => {
-    // const result = await dbService.transaction.add([{ to: '0x1', from: '0x2', amount: 1, fee: 0.01 }, { to: '0x1', from: '0x2', amount: 2, fee: 0.01 }, { to: '0x1', from: '0x2', amount: 3, fee: 0.01 }, { to: '0x1', from: '0x2', amount: 4, fee: 0.01 }, { to: '0x1', from: '0x2', amount: 5, fee: 0.01 }]);
-    const result = await ethereumService.getTransactions({ fromBlock: 14450000, toBlock: 14650000, walletAddress: TREASURY_WALLET, tokenContract: USDT });
- 
-    return res.json(result).status(200);
+    const txDetails = await ethereumService.getTransactions({ fromBlock: 14450000, toBlock: 14650000, walletAddress: TREASURY_WALLET, tokenContract: USDT });
+    
+    await dbService.transaction.add(txDetails.transfers);
+
+    return res.json(txDetails).status(200);
   });
 }
--- src/services/ethereum.service.js
+++ src/services/ethereum.service.js
@@ -1,11 +1,9 @@
-const  InputDataDecoder = require("ethereum-input-data-decoder");
 const Web3 = require('web3');
 const { TREASURY_WALLET } = require("../config");
 const { unitByDecimal } = require("../helpers");
 
 const ethereumService = {
   getTransactions: async ({fromBlock, toBlock, walletAddress, tokenContract}) => {
-    const decoder = new InputDataDecoder(tokenContract.ABI);
     const web3 = new Web3(process.env.INFURA_URL);
 
     const tetherContract = new web3.eth.Contract(tokenContract.ABI, tokenContract.ADDRESS);
@@ -36,6 +34,7 @@ const ethereumService = {
       amount: web3.utils.fromWei(m.returnValues.value, unitByDecimal[tokenContract.DECIMAL])
     }));
 
+    //concating send-received transactions
     let transfers = sendTransfers.concat(receivedTransfers);
 
     const promises = [];
@@ -44,12 +43,17 @@ const ethereumService = {
       promises.push(web3.eth.getTransaction(transfers[i].txId));
     }
 
+    //getting tx details async
     let txDetails = await Promise.all(promises);
 
+    //setting tx details to dictionary
     txDetails = Object.assign({}, ...txDetails.map(m => ({ [m.hash]: m })));
 
     transfers = transfers.map(m => 
-      ({...m, fee: txDetails[m.txId].gas * parseFloat(web3.utils.fromWei(txDetails[m.txId].gasPrice, 'ether')) }));
+      ({
+        ...m, 
+        fee: txDetails[m.txId].gas * parseFloat(web3.utils.fromWei(txDetails[m.txId].gasPrice, 'ether')) 
+      }));
 
     const totalPaidFee = transfers.filter(f => f.from === TREASURY_WALLET).reduce((n, { fee }) => n + fee, 0);
 
